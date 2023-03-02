ca3e9fb7f621b553752943eab3750034d15a96cb
Olena-Zhyzhko <97410016+Olena-Zhyzhko@users.noreply.github.com>
2023-03-02 03:15:12 +0200

    add red border on input

--- src/components/ToDoForm.styled.jsx
+++ src/components/ToDoForm.styled.jsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled'
 
 export const Input = styled.input`
     &:focus:invalid {
+        outline: none;
         border:solid 2px #F5192F;
         padding-left: 10px;}
 `;
